5b026802961681f99cfed5455270cc007852d4e0
Andres Santiago Romero <andres.romero1@ibm.com>
2021-05-08 16:13:50 -0500

    generated deployables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCanUf0FX0FiFx53L9i75YCEYZYbN8IEOU',
+    authDomain: 'rule-of-thumb-88375.firebaseapp.com',
+    projectId: 'rule-of-thumb-88375',
+    storageBucket: 'rule-of-thumb-88375.appspot.com',
+    messagingSenderId: '793809402475',
+    appId: '1:793809402475:web:762e5ef27be21c69574f2e',
+    measurementId: 'G-K9ZYWW32JE'
+  }
 };
