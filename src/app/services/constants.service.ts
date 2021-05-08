3fb76985f068800ce5479ba1629e7a9ea16ca79d
Andres Santiago Romero <andres.romero1@ibm.com>
2021-05-08 16:22:16 -0500

    se cambia src en imágenes

--- src/app/services/constants.service.ts
+++ src/app/services/constants.service.ts
@@ -5,8 +5,8 @@ import { Injectable } from '@angular/core';
 })
 export class ConstantsService {
 
-  public THUMB_UP_IMAGE_URL = '/assets/img/thumbs-up.svg';
-  public THUMB_DOWN_IMAGE_URL = '/assets/img/thumbs-down.svg';
+  public THUMB_UP_IMAGE_URL = 'assets/img/thumbs-up.svg';
+  public THUMB_DOWN_IMAGE_URL = 'assets/img/thumbs-down.svg';
 
   constructor() { }
 }
